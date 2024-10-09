34b76facccac61945b400ef83830775c4de371bd
HenriqueMartinelliPinheiro <henriquepinheiro18@gmail.com>
2024-10-09 20:31:57 -0300

    rota dos docentes

--- app.js
+++ app.js
@@ -6,11 +6,14 @@ const cursosRouter = require('./cursos');
 const discentesRouter = require('./discentes');
 const turmasRouter = require('./turmas');
 const participantesRouter = require('./participantes');
+const docentesRouter = require('./docentes');
 
 app.use(cursosRouter);
 app.use(discentesRouter);
 app.use(turmasRouter)
 app.use(participantesRouter);
+app.use(docentesRouter);
+
 
 app.get('/', (req, res) => {
   res.send('Bem-vindo à API de Cursos e Discentes');
