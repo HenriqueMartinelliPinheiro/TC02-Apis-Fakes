953186c5d6678eeace233e178f3ada01f9bbb400
HenriqueMartinelliPinheiro <henriquepinheiro18@gmail.com>
2024-10-14 19:42:44 -0300

    atualizacao

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 5000;
 
 const cursosRouter = require('./cursos');
 const discentesRouter = require('./discentes');
